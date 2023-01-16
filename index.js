95c7ded1019a2e31be87284091874dfe8c2d1de7
karthicrajan <karthicrajan2002tpt@gmail.com>
2023-01-16 16:31:16 +0530

    url  fix commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const manageUser = require("./routes/manageUser");
 const path = require("path"); 
 const cors = require('cors');
 app.use(cors({
-    origin:"https://talentprojects.netlify.app/",
+    origin:"https://talentprojects.netlify.app",
     // origin:"http://localhost:3000",
 }))
 app.use("/images", express.static(path.join(__dirname,"/images")));
